c8f669f3dd36c7b662ca84d299a991a4627736be
Blare Robinson <berobi01@louisville.edu>
2021-10-16 16:02:46 -0400

    Added API HTML and API controller.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace NFL.Reports
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Calculated Values
+            double ballsCaught;
+
+            ballsCaught = WRResults.Catches;
+
+            Console.WriteLine($"Total Catches: {ballsCaught}");
+        }
+    }
+}

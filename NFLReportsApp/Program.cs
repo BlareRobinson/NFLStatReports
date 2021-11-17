c24fe9e2db03774fd039fd84710b82f9c7a68fcc
Blare Robinson <berobi01@louisville.edu>
2021-11-17 11:38:20 -0500

    I added a menu.

--- NFLReportsApp/Program.cs
+++ NFLReportsApp/Program.cs
@@ -8,18 +8,34 @@ namespace NFLReportsApp
 {
     class Program
     {
+        private static object searchEngine;
+        private static bool keepTrying;
+
         static void Main(string[] args)
         {
 
-            var browseEngine = new NFLBrowseEngine();
-            browseEngine.Initialize();
-                
-            while(true)
+            var searchEngine = new PlayerSearchEngine();
+            // searchEngine.Initialize();
+
+            var keepSearching = true;
+            while (keepSearching)
             {
-                ShowRootMenu();
-                var option = GetRootMenuOption();
+                ShowMenu();
+                var option = GetMenuOption();
                 switch (option)
                 {
+                    case 1:
+                        var allPlayers = searchEngine.GetAllPlayers();
+                        DisplayPlayers(allPlayers);
+                        break;
+                    case 2:
+                        var searchName = GetPlayerSearchName();
+                        var matchingPlayer = searchEngine.SearchPlayers(searchName);
+                        DisplayPlayers(matchingPlayer);
+                        break;
+                    case 3:
+                        keepSearching = false;
+                        return;
 
                 }
 
@@ -27,20 +43,96 @@ namespace NFLReportsApp
 
         }
 
-        private static object GetRootMenuOption()
+        private static void DisplayPlayers(object allPlayers)
         {
             throw new NotImplementedException();
         }
 
-        private static void ShowRootMenu()
+        public class Root
         {
-            throw new NotImplementedException();
+            public List<Player> player { get; set; }
         }
 
-        private static void NFLBrowseEngine()
+        //private static void DisplayPlayers(object allPlayers)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public static void DisplayPlayers(List<Player> allPlayers)
         {
-            throw new NotImplementedException();
+            if (allPlayers.Count == 0)
+            {
+                Console.WriteLine("No players found.");
+            }
+            var i = 0;
+            foreach (var player in allPlayers)
+            {
+                Console.WriteLine($"{++i}) {player.idPlayer}");
+            }
         }
+
+        public static string GetPlayerSearchName()
+
+        {
+
+            while(true)
+            {
+                Console.WriteLine("Enter a players name:");
+                var searchName = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(searchName))
+                {
+                    return searchName;
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid player name {searchName}");
+                }
+            }
+
+
+        }
+
+        public static object GetMenuOption()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (Int32.TryParse(input, out var option))
+                {
+                    return option;
+                }
+                else
+                {                  
+                   // Console.WriteLine($"Invalid player name {option}");
+                    string answer = Console.ReadLine();
+                    if (answer != "Y")
+                    {
+                        keepTrying = false;
+                    }
+                }
+                         
+            }
+        }
+
+        public static void ShowMenu()
+        {
+            // Console.WriteLine("Which NFL team would you like?");
+            var engine = new PlayerSearchEngine();
+           // var Team = Console.ReadLine();
+            Console.WriteLine("1) Get a list of players by their first name");
+            var Name = Console.ReadLine();
+            engine.SearchPlayers(Name);
+            Console.WriteLine("2) Search a player by full name");
+            var fullName = Console.ReadLine();
+            engine.SearchPlayers(Name);
+            Console.WriteLine("3) Would you like to keep searching? (Y/N)");
+            
+            // display a player - return if null - write players to a file use serialize 
+
+
+        }
+
+        
     }
 }
 

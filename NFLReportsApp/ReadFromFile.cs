6e1fe4753e6f38cd7eb8e58c42c763ba19fa4ca0
Blare Robinson <berobi01@louisville.edu>
2021-11-18 20:30:52 -0500

    Upated the File.txt

--- NFLReportsApp/ReadFromFile.cs
+++ NFLReportsApp/ReadFromFile.cs
@@ -11,6 +11,8 @@ namespace NFLReportsApp
             try
             {
                 // Open the text file using a stream reader.
+                string fileName = "Cleveland.txt";
+                string path = Path.Combine(Environment.CurrentDirectory, @"NFLReportsApp\", fileName);
                 using (var sr = new StreamReader("Cleveland.txt"))
                 {
                     // Read the stream as a string, and write the string to the console.

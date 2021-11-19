62c8742da880cc879cbacec7c0fff66aa55f2eeb
Blare Robinson <berobi01@louisville.edu>
2021-11-19 13:22:40 -0500

    Backlash was accidently deleted when deleting commented out notes.

--- NFLReportsApp/ReadFromFile.cs
+++ NFLReportsApp/ReadFromFile.cs
@@ -10,12 +10,12 @@ namespace NFLReportsApp
         {
             try
             {
-                // Open the text file using a stream reader.
+                
                 string fileName = "Cleveland.txt";
-                string path = Path.Combine(Environment.CurrentDirectory, @"NFLReportsApp", fileName);
+                string path = Path.Combine(Environment.CurrentDirectory, @"NFLReportsApp\", fileName);
                 using (var sr = new StreamReader(path))
                 {
-                    // Read the stream as a string, and write the string to the console.
+                    
                     Console.WriteLine(sr.ReadToEnd());
                 }
             }
